37d34f706f592a1e79cdb2e8b3e6d2d601d56f83
Alexander A Lizarraga <alexander_lizarraga@ymail.com>
2018-06-20 00:49:14 -0400

    working on splitting divs

--- src/components/DisplayInfo/DisplayInfo.js
+++ src/components/DisplayInfo/DisplayInfo.js
@@ -4,12 +4,33 @@ const titles = ["JS", "MongoDB", "Deep Learning", "React", "Full Stack", "Fun", 
 const descriptions = "";
 
 class DisplayInfo extends Component {
-	render() {
+	constructor(props) {
+		super(props);
+		this.state = { displayInfoResize: 49.5 };
+
+		// This binding is necessary to make `this` work in the callback
+		this.handleClick = this.handleClick.bind(this);
+		this.handleMouseLeave = this.handleMouseLeave.bind(this);
+	}
+
+	handleClick(e) {
+		// console.log(e.target.style.width); //check target width
+		console.log(this);
+		e.target.style.width = "90vw";
+		this._reactInternalFiber.child.child.stateNode.nextSibling.style.width = "8vw";
+		// this.setState({ displayInfoResize: 99 });
+	}
+	handleMouseLeave(e) {
+		e.target.style.width = "49.5vw";
+		this._reactInternalFiber.child.child.stateNode.nextSibling.style.width = "49.5vw";
+		// this.setState({ displayInfoResize: 49.5 });
+	}
+	render(props) {
 		return (
-			<section className="displayInfoContainer row b-0 m-0 p-2">
+			<section className="displayInfoContainer b-0 m-0 p-2" >
 				{
 					titles.map((title, index) =>
-						<div className="displayInfoCard col-6 mx-0 p-0">
+						<div className={"displayInfoCard mx-0 p-0"} Style={"width:" + this.state.displayInfoResize + "vw;"} onMouseOver={this.handleClick} onMouseOut={this.handleMouseLeave}>
 							<p>{title}</p>
 							<p>A small project using JS</p>
 						</div>
